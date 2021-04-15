fe0979dd5f14e00c94402ad36bcc1e1dbc4f97d9
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2021-04-15 16:05:07 -0300

    creo componente atomo Price

--- components/atoms/Price/index.jsx
+++ components/atoms/Price/index.jsx
@@ -1,7 +1,7 @@
-import './styles.module.scss';
+import styles from './styles.module.scss';
 
-const Price = () => {
-  return <p>$500</p>
+const Price = ({ size = 'lg', price = '$500' }) => {
+	return <p className={`${styles.customPrice} ${styles[size]}`}>{price}</p>;
 };
 
 export default Price;
