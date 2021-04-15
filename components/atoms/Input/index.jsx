474bdab6514b7ce161ffeb3f764d27fe3f65f43f
Sergio Correa <felizjanuk12@gmail.com>
2021-04-14 22:14:32 -0500

    atomo input creado

--- components/atoms/Input/index.jsx
+++ components/atoms/Input/index.jsx
@@ -1,11 +1,38 @@
-import './styles.module.scss';
+import s from './styles.module.scss';
 
-const Input = () => {
-  return (
-    <>
-      <p>Input</p>
-    </>
-  );
+function selectClassName({ size }) {
+	let cn;
+
+	// Check the size passed into the props and assign a class
+	if (size === 'lg') {
+		cn = 'lg';
+	} else if (size === 'md') {
+		cn = 'md';
+	} else if (size === 'sm') {
+		cn = 'sm';
+	}
+
+	return cn;
+}
+
+const Input = ({
+	size = 'lg',
+	isDisabled = false,
+	type = 'text',
+	placeholder,
+	onChange,
+}) => {
+	const cn = selectClassName({ size });
+
+	return (
+		<input
+			className={`${s.customInput} ${s[cn]}`}
+			type={type}
+			placeholder={placeholder}
+			onChange={onChange}
+			disabled={isDisabled}
+		/>
+	);
 };
 
 export default Input;
