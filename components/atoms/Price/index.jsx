0e945a4e1b4fe67497b51b03bb5e3ae61e67ab77
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2021-04-15 22:09:20 -0300

    agrego moneda como prop separada

--- components/atoms/Price/index.jsx
+++ components/atoms/Price/index.jsx
@@ -1,7 +1,7 @@
 import styles from './styles.module.scss';
 
-const Price = ({ size = 'lg', price = '$500' }) => {
-	return <p className={`${styles.customPrice} ${styles[size]}`}>{price}</p>;
+const Price = ({ size = 'lg', price = '500', coin='$' }) => {
+	return <p className={`${styles.customPrice} ${styles[size]}`}>{`${coin}${price}`}</p>;
 };
 
 export default Price;
