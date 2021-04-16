4bd1861c29f2b0f78c946fa288660c8d2134fa2a
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2021-04-15 22:18:34 -0300

    corrigo coin por currency

--- components/atoms/Price/index.jsx
+++ components/atoms/Price/index.jsx
@@ -1,7 +1,7 @@
 import styles from './styles.module.scss';
 
-const Price = ({ size = 'lg', price = '500', coin='$' }) => {
-	return <p className={`${styles.customPrice} ${styles[size]}`}>{`${coin}${price}`}</p>;
+const Price = ({ size = 'lg', price = '500', currency='$' }) => {
+	return <p className={`${styles.customPrice} ${styles[size]}`}>{`${currency}${price}`}</p>;
 };
 
 export default Price;
