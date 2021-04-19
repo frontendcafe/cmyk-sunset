cad464669e19ab52a553df261696d904d26dccba
angela-goncalves <angelakgo20@gmail.com>
2021-04-19 18:01:57 -0300

    agregada nueva variedad del boton

--- components/atoms/Button/index.jsx
+++ components/atoms/Button/index.jsx
@@ -1,12 +1,14 @@
 import styles from './styles.module.scss'; 
 
-const Button = ({size, disabled, children}) => {  
+const Button = ({size, disabled, children, color}) => {  
 
   const btnSize = size === "lg" ? styles.lg : size === "md" ? styles.md : size === "sm" ? styles.sm : ""
 
+  const colorbg = color === "green" ? styles.green : color === "red" ? styles.red : "" 
+
   return (
     <button 
-      className={`${btnSize} ${styles.btn}`} 
+      className={`${btnSize} ${styles.btn} ${colorbg}`} 
       disabled={disabled}
     >{children}
     </button>
