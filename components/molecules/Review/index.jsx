25ab43dd78680b1b74ad8ac276c053e64758c2e5
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2021-04-21 01:08:54 -0300

    agrego responsividad con media queries

--- components/molecules/Review/index.jsx
+++ components/molecules/Review/index.jsx
@@ -1,12 +1,23 @@
 import styles from './styles.module.scss';
+import Paragraph from 'components/atoms/Paragraph';
 
-const Review = ({name='Galindez', rating='7.6 de 10', comment='"Gran Comic"', size='desktop'}) => {
-  return (
-    <div className={`${styles.box} ${styles[size]}`}>
-      <p className={`${styles.nameRating}`}>{`${name} - ${rating}`}</p>
-      <p  className={`${styles.comment}`}>{comment}</p>
-    </div>
-  );
+const Review = ({
+	name = 'Galindez',
+	rating = '7.6 de 10',
+	comment = '"Gran Comic"',
+}) => {
+	return (
+		<div className={styles.box}>
+			<div className={styles.firstLine}>
+				<Paragraph content={name} />
+				<Paragraph content={'-'} />
+				<Paragraph content={rating} />
+			</div>
+			<div className={styles.secondLine}>
+				<Paragraph content={comment} />
+			</div>
+		</div>
+	);
 };
 
 export default Review;
