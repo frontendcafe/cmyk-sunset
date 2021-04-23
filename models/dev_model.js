3f5276606c6f5a8f321d46a2ce39552b92bd5a40
german paak <germanpaak@gmail.com>
2021-04-23 19:43:54 -0300

    Modelo de dev.

---
+++ models/dev_model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const devShema = new mongoose.Schema({
+	// _id: mongoose.Schema.Types.ObjectId,
+	nombre: { type: String, required: true },
+	apellido: { type: String, required: true },
+	nick: { type: String, required: true },
+	url: String,
+	linkedin: String,
+	github: String,
+	twitter: String,
+	imagen: String,
+	discord: String,
+	tags: [
+		{
+			code: String,
+		},
+	],
+});
+
+module.exports = mongoose.models.Dev || mongoose.model('Dev', orderShema);
