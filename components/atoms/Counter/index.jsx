98c337f3d5d355dae1eb373a89a15228b6d8c302
Ignacio Diaz <diaz.ignacio023@gmail.com>
2021-04-23 12:53:01 -0300

    creo átomo counter

---
+++ components/atoms/Counter/index.jsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss';
+
+const Counter = ({ quantity = 0 }) => {
+	return (
+		<>
+			<p className={styles.counter}>{quantity}</p>
+		</>
+	);
+};
+
+export default Counter;
