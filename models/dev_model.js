4e5d2768b10af5c842e253dbf5f6df6d8f9bb446
german paak <germanpaak@gmail.com>
2021-04-24 15:24:10 -0300

    delete comment

--- models/dev_model.js
+++ models/dev_model.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const devShema = new mongoose.Schema({
-	// _id: mongoose.Schema.Types.ObjectId,
 	nombre: { type: String, required: true },
 	apellido: { type: String, required: true },
 	nick: { type: String, required: true },
