4135b7931e1d0162827bf626415245b48b3d275b
angela-goncalves <angelakgo20@gmail.com>
2021-04-25 16:23:44 -0300

    agregados los estilos al carrito

--- components/organisms/Cart/index.jsx
+++ components/organisms/Cart/index.jsx
@@ -1,16 +1,31 @@
 import Button from 'components/atoms/Button/index'
 import Paragraph from 'components/atoms/Paragraph/index'
+import Price from 'components/atoms/Price/index'
 import ItemCart from 'components/molecules/ItemCart/index'
+import React, { useState } from 'react'
+import styles from './styles.module.scss';
 
 //mismo fondo y organizar los items en el centro de la pantalla
 
-const Cart = ({}) =>{
-    return(
-        <div>
-            <ItemCart></ItemCart>
-            <Paragraph content='Total: '></Paragraph>
-            <Button size="md">Comprar</Button>
-        </div>
+const Cart = () => {
+
+    const [counter, setCounter] = useState("")
+
+    return (
+        <div className={`${styles.container}`}>
+            <ItemCart />
+            <div className={`${styles.buyItems}`}>
+                <div className={`${styles.price}`}>
+                    <Paragraph content='Total: ' />
+                    <Price
+                        size='lg'
+                        price='500'
+                        currency='$'
+                    />
+                </div>
+                <Button size="md" color={"green"}>Comprar</Button>
+            </div>
+        </div >
     );
 }
 
