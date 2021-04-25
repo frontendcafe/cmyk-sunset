abdc2f86f5759d55fe02842dbd505a251133f4b0
Ignacio Diaz <diaz.ignacio023@gmail.com>
2021-04-24 21:52:09 -0300

    creo atomo icon

--- components/atoms/Icon/index.jsx
+++ components/atoms/Icon/index.jsx
@@ -1,11 +1,19 @@
-import './styles.module.scss';
+import styles from './styles.module.scss';
+import { CgShoppingBag } from 'react-icons/cg';
+import Counter from 'components/atoms/Counter';
 
-const Icon = () => {
-  return (
-    <>
-      <p>Icon</p>
-    </>
-  );
+const Icon = ({ isDisabled = false, hasCounter = false, dark = false }) => {
+	let cn = hasCounter ? styles.hasCounter : '';
+	cn += dark ? styles.dark : '';
+	const counter = hasCounter ? <Counter /> : null;
+	return (
+		<>
+			<button disabled={isDisabled} className={`${styles.button} ${cn}`}>
+				<CgShoppingBag className={styles.icon} />
+				{counter}
+			</button>
+		</>
+	);
 };
 
 export default Icon;
