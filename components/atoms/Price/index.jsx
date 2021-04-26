b14088414bd4aebca9318891fec4707aab5beb2e
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2021-04-26 16:04:29 -0300

    Update de atomos price, paragraph y title

--- components/atoms/Price/index.jsx
+++ components/atoms/Price/index.jsx
@@ -1,7 +1,7 @@
 import styles from './styles.module.scss';
 
-const Price = ({ size = 'lg', price = '500', currency='$' }) => {
-	return <p className={`${styles.customPrice} ${styles[size]}`}>{`${currency}${price}`}</p>;
+const Price = ({ size = 'lg', price = '500', currency='$', className }) => {
+	return <span className={`${styles.customPrice} ${styles[size]} ${className}`}>{`${currency}${price}`}</span>;
 };
 
 export default Price;
