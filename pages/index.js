de59bbb96fe48f1546d35f751c41e92bcde79d7b
german paak <germanpaak@gmail.com>
2021-04-28 13:33:50 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,5 @@
 import ComicCategory from 'components/molecules/ComicCategory/index';
 
 export default function Home() {
-	const data = [
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-		{ src: '/capitanamerica.jpg', alt: 'Capitan amercia' },
-	];
-
-	const handlerClick = comic => {
-		console.log(comic);
-	};
-
-	return (
-		<ComicCategory title='Avengers' comics={data} onClick={handlerClick} />
-	);
+	return 'Home';
 }
