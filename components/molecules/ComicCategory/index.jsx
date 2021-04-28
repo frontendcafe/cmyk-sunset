5f834e1fd24f74a561943690f0c33e5bd6c72a50
german paak <germanpaak@gmail.com>
2021-04-28 11:38:01 -0300

    Click y ajustes mobile

--- components/molecules/ComicCategory/index.jsx
+++ components/molecules/ComicCategory/index.jsx
@@ -3,14 +3,24 @@ import Title from 'components/atoms/Title/index';
 import styles from './styles.module.scss';
 import React from 'react';
 
-const ComicCategory = ({ title = 'title', comics, size = 'md', className }) => {
+const ComicCategory = ({
+	title = 'title',
+	comics,
+	size = 'md',
+	className,
+	onClick,
+}) => {
+	console.log('estamos');
 	return (
 		<div>
 			<Title size={size} content={title} className={styles.title} />
 			<div className={styles.wrapper}>
 				{comics.map((comic, key) => {
 					return (
-						<div key={key + 'i'} className={styles.img}>
+						<div
+							key={key + 'i'}
+							className={styles.img}
+							onClick={() => onClick(comic)}>
 							<Image key={key} src={comic.src} alt={comic.alt} />
 						</div>
 					);
