b782d78fe194bbb56f94643226b31cb09d7ea6f4
german paak <germanpaak@gmail.com>
2021-04-28 10:16:23 -0300

    imagen mobile en 50%

--- components/molecules/ComicCategory/index.jsx
+++ components/molecules/ComicCategory/index.jsx
@@ -6,16 +6,11 @@ import React from 'react';
 const ComicCategory = ({ title = 'title', comics, size = 'md', className }) => {
 	return (
 		<div>
-			<Title
-				size={size}
-				content={title}
-				className={styles.title}
-			/>
-
+			<Title size={size} content={title} className={styles.title} />
 			<div className={styles.wrapper}>
 				{comics.map((comic, key) => {
 					return (
-						<div className={styles.img}>
+						<div key={key + 'i'} className={styles.img}>
 							<Image key={key} src={comic.src} alt={comic.alt} />
 						</div>
 					);
