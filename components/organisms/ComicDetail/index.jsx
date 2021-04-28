95c6ccb5e0b8515a82192fed10daee34a55f39d8
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2021-04-28 16:30:33 -0300

    Update index.jsx

--- components/organisms/ComicDetail/index.jsx
+++ components/organisms/ComicDetail/index.jsx
@@ -19,19 +19,3 @@ const ComicDetail = () => {
 };
 
 export default ComicDetail;
-
-// size, disabled, children, color
-// defaultSize = 'md',
-// 	title = 'X-Men Legends #2',
-// 	rating = '4/10',
-//   ratingSize = 'md',
-// 	src = '',
-// 	currency = '$',
-// 	price = '500',
-//   priceSize = 'lg'
-
-// defaultSize = 'sm',
-// 	title = 'Información del comic',
-// 	content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet, leo ut hendrerit consectetur, elit elit posuere orci, ac feugiat libero nisi eu eros. Quisque scelerisque tortor orci, id dapibus ligula ultrices a. Aenean rhoncus risus nec sapien placerat, id rhoncus magna suscipit.',
-// 	pages = '152',
-// 	creators = ['Lorem Ipsum', 'Lorem Ipsum', 'Lorem Ipsum'],
