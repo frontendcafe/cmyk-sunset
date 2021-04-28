5b7ff4cae05ee214c3f55cc5c4b234f43831e0d7
Agustin Vazquez <agustin.vazquez@wolox.com.ar>
2021-04-28 18:24:03 -0300

    hago integracion con el backend

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
-import ComicCategory from 'components/molecules/ComicCategory/index';
+import { useEffect, useState } from "react";
 
 export default function Home() {
-	return 'Home';
+	const [comics, setComics] = useState([]);
+
+	useEffect(() => {
+		fetch('/api/comic')
+		.then(response => response.json())
+		.then(data => setComics(data));
+	});
+
+	return (
+		<ul>
+			{comics.map((comic, index) => (
+				<li key={index}>{comic.title}</li>
+			))}
+		</ul>
+	);
 }
