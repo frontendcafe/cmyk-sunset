e160768a13181ab8fb837439646ff14e22abf1fd
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2021-04-30 16:14:32 -0300

    Update index.jsx

--- components/organisms/ComicDetail/index.jsx
+++ components/organisms/ComicDetail/index.jsx
@@ -3,17 +3,48 @@ import ComicInfo from 'components/molecules/ComicInfo';
 import ComicDescription from 'components/molecules/ComicDescription';
 import Button from 'components/atoms/Button';
 
-const ComicDetail = () => {
+const ComicDetail = ({
+	infoSize = 'md',
+	infoTitle = 'X-Men Legends #2',
+	rating = '4/10',
+	ratingSize = 'md',
+	src = '',
+	currency = '$',
+	price = '500',
+	priceSize = 'lg',
+	descrSize = 'sm',
+	descrTitle = 'Información del comic',
+	content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet, leo ut hendrerit consectetur, elit elit posuere orci, ac feugiat libero nisi eu eros. Quisque scelerisque tortor orci, id dapibus ligula ultrices a. Aenean rhoncus risus nec sapien placerat, id rhoncus magna suscipit.',
+	pages = '152',
+	creators = ['Lorem Ipsum', 'Lorem Ipsum', 'Lorem Ipsum'],
+}) => {
 	return (
 		<div className={styles.comicDetail}>
-			<ComicInfo className={styles.comicInfo} />
+			<ComicInfo
+				className={styles.comicInfo}
+				size={infoSize}
+				title={infoTitle}
+				rating={rating}
+				ratingSize={ratingSize}
+				src={src}
+				currency={currency}
+				price={price}
+				size={priceSize}
+			/>
 			<Button
 				className={styles.buyButton}
 				size='md'
 				color='green'
 				children='AGREGAR AL CARRITO'
 			/>
-			<ComicDescription className={styles.comicDescr} />
+			<ComicDescription
+				className={styles.comicDescr}
+				size={descrSize}
+				title={descrTitle}
+				content={content}
+				pages={pages}
+				creators={creators}
+			/>
 		</div>
 	);
 };
