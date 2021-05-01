a592b3007a54c903a46c36a9d86ba706b8fddc28
angela-goncalves <angelakgo20@gmail.com>
2021-05-01 20:15:04 -0300

    agregadas props al input para el formulario

--- components/atoms/Input/index.jsx
+++ components/atoms/Input/index.jsx
@@ -7,15 +7,26 @@ const Input = ({
 	placeholder,
 	onChange,
 	dark = false,
+	required,
+	minLength,
+	maxLength,
+	pattern,
+	className,
+	value,
 }) => {
 	const cn = dark ? styles.dark : '';
 	return (
 		<input
-			className={`${styles.customInput} ${styles[size]} ${cn}`}
+			className={`${styles.customInput} ${styles[size]} ${cn} ${className}`}
 			type={type}
 			placeholder={placeholder}
 			onChange={onChange}
 			disabled={isDisabled}
+			required={required}
+			minLength={minLength}
+			maxLength={maxLength}
+			pattern={pattern}
+			value={value}
 		/>
 	);
 };
