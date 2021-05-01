1e687b0fa3afb5d59aab3a3a6bd1d57be6aa3f17
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2021-05-01 17:25:20 -0300

    agrego elementos ocultos según breakpoints

--- components/organisms/Suggestions/index.jsx
+++ components/organisms/Suggestions/index.jsx
@@ -1,20 +1,27 @@
 import styles from './styles.module.scss';
-import Subtitle from 'components/atoms/Subtitle';
+import Title from 'components/atoms/Title';
 import Suggestion from 'components/molecules/Suggestion';
 
 const Suggestions = ({ list }) => {
 	return (
 		<div className={styles.suggestions}>
-			<div className={styles.subtitleBox}>
-				<Subtitle
-					children='Una sugerencia para ti'
+			<div className={styles.titleBox}>
+				<Title
+					content='Una sugerencia para ti'
 					size='sm'
-					className={styles.subtitle}
+					className={styles.title}
+				/>
+				<Title
+					content='Unas sugerencias para ti'
+					size='sm'
+					className={styles.titlePlural}
 				/>
 			</div>
 
 			<div className={styles.suggestionsBox}>
 				<Suggestion className={styles.suggestion} />
+				<Suggestion className={`${styles.suggestion} ${styles.hidden}`} />
+				<Suggestion className={`${styles.suggestion} ${styles.hidden}`} />
 			</div>
 		</div>
 	);
