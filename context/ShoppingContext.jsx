f26ba015e78c47b9dcf0e1c93b2f1901dfcf6958
german paak <germanpaak@gmail.com>
2021-05-02 12:46:18 -0300

    estructura del context

---
+++ context/ShoppingContext.jsx
@@ -0,0 +1,83 @@
+import React, { createContext } from 'react';
+
+export const ShoppingContext = React.createContext('');
+
+const orderData = {
+	userName: null,
+	totalAmount: 0,
+	itemsCount: 0,
+	orderStatus: 'Pending',
+	payMethod: null,
+	payId: null,
+	payStatus: 'Pending',
+	deliveryMethod: null,
+	deliveryAdress: null,
+	delivered: null,
+	finished: false,
+	createdAt: null,
+	updatedAt: null,
+	items: [],
+	// 	{
+	// 		id_item: mongoose.Schema.Types.ObjectId,
+	// 		idMarve: String,
+	// 		title: String,
+	// 		quantity: Number,
+	// 		price: Number,
+	// 		total: Number,
+	// 	},
+	// ],
+};
+
+export const ShoppingProvider = ({ children }) => {
+	const [cart, setCart] = React.useState([]);
+
+	const setCart = user => {
+		const order = orderData;
+		order.userName = user.userName;
+		order.createdAt = Date.now();
+		order.updatedAt = Date.now();
+		localStorage.setItem('cart', JSON.stringify(order));
+		setCart(order);
+		return order;
+	};
+
+	const getCart = () => {
+		const order = JSON.parse(localStorage.getItem('cart'));
+		return order;
+	};
+
+	const addItemCart = item => {
+		console.log(item);
+		return item;
+	};
+
+	const removeItemCart = item => {
+		// localStorage.removeItem('logged');
+		return true;
+	};
+
+	const setPayDelivery = order => {
+		// TODO
+
+		return order;
+	};
+
+	const checkout = () => {
+		// Todo
+	};
+
+	return (
+		<ShoppingContext.Provider
+			value={{
+				cart,
+				setCart,
+				getCart,
+				addItemCart,
+				removeItemCart,
+				setPayDelivery,
+				checkout,
+			}}>
+			{children}
+		</ShoppingContext.Provider>
+	);
+};
