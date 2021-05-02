6c7dc4d247da17fab76f70e8c606a2429db62c3f
Ignacio Diaz <diaz.ignacio023@gmail.com>
2021-05-02 13:19:36 -0300

    creo sidebar

---
+++ components/molecules/Sidebar/index.jsx
@@ -0,0 +1,94 @@
+import styles from './styles.module.scss';
+import Title from 'components/atoms/Title';
+import Subtitle from 'components/atoms/Subtitle';
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
+
+//
+// MOVER CROSS A NAVBAR
+//
+
+const Sidebar = ({ isLoggedIn, className = '', menuCollapse }) => {
+	const active = menuCollapse ? styles.active : '';
+
+	let login = null;
+	if (isLoggedIn) {
+		login = (
+			<li>
+				<Link href='/sign-out'>
+					<a>
+						<Subtitle
+							size={'lg'}
+							children={'Sign Out'}
+							className={styles.subtitle}
+						/>
+					</a>
+				</Link>
+			</li>
+		);
+	} else {
+		login = (
+			<>
+				<li>
+					<Link href='/sign-up'>
+						<a>
+							<Subtitle
+								size={'lg'}
+								children={'Sign Up'}
+								className={styles.subtitle}
+							/>
+						</a>
+					</Link>
+				</li>
+				<li>
+					<Link href='/login'>
+						<a>
+							<Subtitle
+								size={'lg'}
+								children={'Login'}
+								className={styles.subtitle}
+							/>
+						</a>
+					</Link>
+				</li>
+			</>
+		);
+	}
+
+	return (
+		<div className={`${styles.sidebar} ${active}`}>
+			<div className={styles.items}>
+				<span className={styles.head}>
+					<Title size={'lg'} children='Name' hasBg />
+				</span>
+				<ul>
+					<li>
+						<Link href='/'>
+							<a>
+								<Subtitle
+									size={'lg'}
+									children='Home'
+									className={styles.subtitle}
+								/>
+							</a>
+						</Link>
+					</li>
+					<li>
+						<Link href='/about-us'>
+							<a>
+								<Subtitle
+									size={'lg'}
+									children='About Us'
+									className={styles.subtitle}
+								/>
+							</a>
+						</Link>
+					</li>
+					{login}
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default Sidebar;
