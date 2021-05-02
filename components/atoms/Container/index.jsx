cd17e085c7a5cb558c277c61be24f5b030378876
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-05-02 12:24:11 -0300

    muevo el navbar al container

--- components/atoms/Container/index.jsx
+++ components/atoms/Container/index.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import withLoading from 'components/atoms/WithLoading';
+import { ComicContext } from 'context/ComicContext';
+import Navbar from "components/molecules/Navbar/index";
 
 import styles from './styles.module.scss';
 
-function Container({children}) {
-  return (
-    <div className={styles.container}>
-      {children}
-    </div>
-  )
+function Container({ children, hasSearch }) {
+	const { filterByText } = useContext(ComicContext);
+
+	const handleSearch = event => filterByText(event.target.value);
+
+	return (
+		<>
+			<Navbar onChange={handleSearch} hasSearch={hasSearch}/>
+			<div className={styles.container}>{children}</div>
+		</>
+	);
 }
 
 export default withLoading(Container);
