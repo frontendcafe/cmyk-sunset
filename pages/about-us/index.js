425c3deb0dd5673ed8a195f92a73a6c73daecd7e
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-05-02 13:14:53 -0300

    pagina about-us

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -1,3 +1,25 @@
+import Container from 'components/atoms/Container/index';
+import AppInfo from 'components/molecules/AppInfo';
+import Devs from 'components/organisms/Devs';
+import { useEffect, useState } from 'react';
+
 export default function AboutUs() {
-  return "AboutUs";
+  const [loading, setLoading] = useState(true);
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/devs')
+    .then(response => response.json())
+    .then(jsonResponse => {
+      setDevs(jsonResponse.data);
+      setLoading(false);
+    })
+  }, []);
+
+  return (
+    <Container hasNavbar={false} loading={loading}>
+      <AppInfo />
+      <Devs devs={devs}/>
+    </Container>
+  );
 }
