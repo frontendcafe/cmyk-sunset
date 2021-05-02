020ec372d0494dba950c4d2a69a0dfef67cf67a9
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-05-02 20:27:29 -0300

    integro sesion al sidebar

--- components/molecules/Sidebar/index.jsx
+++ components/molecules/Sidebar/index.jsx
@@ -1,59 +1,48 @@
+import Link from 'next/link';
+import { useContext } from 'react';
+
 import styles from './styles.module.scss';
 import Title from 'components/atoms/Title';
 import Subtitle from 'components/atoms/Subtitle';
-import Link from 'next/link';
+import { DataContext } from 'context/DataContext';
 
-const Sidebar = ({ isLoggedIn, menuCollapse }) => {
+const Sidebar = ({ menuCollapse }) => {
 	const active = menuCollapse ? styles.active : '';
+	const { isLogged, data, logOut } = useContext(DataContext);
 
 	let login = null;
-	if (isLoggedIn) {
+	if (isLogged()) {
 		login = (
 			<li>
-				<Link href='/sign-out'>
-					<a>
-						<Subtitle
-							size={'lg'}
-							children={'Sign Out'}
-							className={styles.subtitle}
-						/>
-					</a>
-				</Link>
+				<Subtitle
+					size={'lg'}
+					className={styles.subtitle}
+					onClick={logOut}
+				>
+					Sign Out
+				</Subtitle>
 			</li>
 		);
 	} else {
 		login = (
-			<>
-				<li>
-					<Link href='/sign-up'>
-						<a>
-							<Subtitle
-								size={'lg'}
-								children={'Sign Up'}
-								className={styles.subtitle}
-							/>
-						</a>
-					</Link>
-				</li>
-				<li>
-					<Link href='/login'>
-						<a>
-							<Subtitle
-								size={'lg'}
-								children={'Login'}
-								className={styles.subtitle}
-							/>
-						</a>
-					</Link>
-				</li>
-			</>
+			<li>
+				<Link href='/login'>
+					<a>
+						<Subtitle size={'lg'} className={styles.subtitle}>
+							Login
+						</Subtitle>
+					</a>
+				</Link>
+			</li>
 		);
 	}
 
 	return (
 		<div className={`${styles.sidebar} ${active}`}>
 			<div className={styles.items}>
-				<Title size={'lg'} children='Name' hasBg />
+				<Title size={'lg'} hasBg>
+					{data?.name || "Marvel Store"}
+				</Title>
 				<ul>
 					<li>
 						<Link href='/'>
