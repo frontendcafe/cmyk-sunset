523c005c95624fd237485b80de8bee0f9301aa19
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-05-02 12:24:19 -0300

    actualizo home

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,18 @@
 import { useContext } from "react";
 
 import { ComicContext } from 'context/ComicContext';
-import Navbar from "components/molecules/Navbar/index";
 import Suggestions from "components/organisms/Suggestions/index";
 import ComicCategory from "components/molecules/ComicCategory/index";
 
 import Container from 'components/atoms/Container';
 
 export default function Home() {
-	const { loading, filteredComics, recommendations, filterByText } = useContext(ComicContext);
-
-	const handleSearch = (event) => filterByText(event.target.value);
+	const { loading, filteredComics, recommendations } = useContext(ComicContext);
 
 	return (
-		<>
-			<Navbar onChange={handleSearch} />
-			<Container loading={loading}>
-				<Suggestions list={recommendations} />
-				<ComicCategory title="Comics" comics={filteredComics} />
-			</Container>
-		</>
+		<Container loading={loading} hasSearch>
+			<Suggestions list={recommendations} />
+			<ComicCategory title="Comics" comics={filteredComics} />
+		</Container>
 	);
 }
