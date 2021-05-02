9e6bc606fdec9334fff817a06310a4fcd6a8f1b0
Agustin Vazquez <agustin.vazquez@wolox.com.ar>
2021-05-01 21:41:43 -0300

    continuo construyendo el home

--- pages/index.js
+++ pages/index.js
@@ -5,21 +5,20 @@ import Navbar from "components/molecules/Navbar/index";
 import Suggestions from "components/organisms/Suggestions/index";
 import ComicCategory from "components/molecules/ComicCategory/index";
 import Loader from 'components/atoms/Loader';
+import Container from 'components/atoms/Container';
 
 export default function Home() {
-	const { loading, filteredComics, getRecommendation, filterByText } = useContext(ComicContext);
-
-	console.log(filteredComics);
+	const { loading, filteredComics, recommendations, filterByText } = useContext(ComicContext);
 
 	return (
 		<>
 			<Navbar />
 			{
 				loading ? <Loader /> :
-				<>
-					<Suggestions />
+				<Container>
+					<Suggestions list={recommendations}/>
 					<ComicCategory title="Comics" comics={filteredComics}/>
-				</>
+				</Container>
 			}
 		</>
 	);
