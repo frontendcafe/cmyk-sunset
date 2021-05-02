f06506b8d13aa61a2e1a3894cf9a7655f9661454
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-05-02 12:24:44 -0300

    pagina de detalle de comic

--- pages/comic/[id].js
+++ pages/comic/[id].js
@@ -1,6 +1,39 @@
 import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react';
 
-export default function ComicDetail() {
-  const router = useRouter()
-  return `ComicDetail: ${router.query.id}`;
+import { getRandomInt } from 'util/getRandomInt';
+import ComicDetail from 'components/organisms/ComicDetail';
+import Container from 'components/atoms/Container/index';
+
+export default function Comic() {
+  const { query: { id } } = useRouter();
+  const [comic, setComic] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (id) {
+      fetch(`/api/comic/${id}`)
+        .then(response => response.json())
+        .then(data => {
+          data.imageUrl = `${data.images[0].path}.${data.images[0].extension}`;
+          setComic(data);
+          setLoading(false);
+        })
+    }
+  }, [id]);
+
+  return (
+    <Container loading={loading}>
+      <ComicDetail
+        infoTitle={comic?.title}
+        rating={`${getRandomInt(0, 10)}/10`}
+        src={comic?.imageUrl}
+        currency='$'
+        price={comic?.prices?.[0].price || getRandomInt(200, 600)}
+        content={comic?.description}
+        pages={comic?.pageCount || getRandomInt(100, 200)}
+        creators={comic?.creators?.items.map(({ name }) => name)}
+      />
+    </Container>
+  );
 }
