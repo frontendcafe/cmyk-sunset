9c7e5050d0f502d6cee461bf9d853af0fa43a12a
angela-goncalves <angelakgo20@gmail.com>
2021-05-05 10:27:25 -0300

    mejoras en los cambios de la solucion de conflictos en el componente button

--- components/atoms/Button/index.jsx
+++ components/atoms/Button/index.jsx
@@ -1,21 +1,19 @@
 import styles from './styles.module.scss';
 
 const Button = ({ size, disabled, children, color, className, onClick, type }) => {
-  const Button = ({ size, disabled, children, color, className, onClick, type }) => {
+  const btnSize = size === "lg" ? styles.lg : size === "md" ? styles.md : size === "sm" ? styles.sm : ""
 
-    const btnSize = size === "lg" ? styles.lg : size === "md" ? styles.md : size === "sm" ? styles.sm : ""
+  const colorbg = color === "green" ? styles.green : color === "red" ? styles.red : ""
 
-    const colorbg = color === "green" ? styles.green : color === "red" ? styles.red : ""
+  return (
+    <button
+      className={`${btnSize} ${styles.btn} ${colorbg} ${className}`}
+      disabled={disabled}
+      onClick={onClick}
+      type={type}
+    >{children}
+    </button>
+  );
+};
 
-    return (
-      <button
-        className={`${btnSize} ${styles.btn} ${colorbg} ${className}`}
-        disabled={disabled}
-        onClick={onClick}
-        type={type}
-      >{children}
-      </button>
-    );
-  };
-
-  export default Button;
+export default Button;
