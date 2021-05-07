bf7ece71d1b4b9269af5ea7950187ec6b55d8ccb
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-05-06 23:16:55 -0300

    agrego mensaje de agregado exitosamente

--- components/molecules/ComicInfo/index.jsx
+++ components/molecules/ComicInfo/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import Title from 'components/atoms/Title';
 import Paragraph from 'components/atoms/Paragraph';
@@ -9,48 +9,65 @@ import { ShoppingContext } from 'context/ShoppingContext';
 
 import styles from './styles.module.scss';
 
+const addComic = 'AGREGAR AL CARRITO';
+const comicAdded = 'AGREGADO AL CARRITO';
+
 const ComicInfo = ({
 	id,
-  defaultSize = 'md',
+	defaultSize = 'md',
 	title = 'X-Men Legends #2',
 	rating = '4/10',
-  ratingSize = 'md',
+	ratingSize = 'md',
 	src = '',
 	currency = '$',
 	price = '500',
-  priceSize = 'lg',
+	priceSize = 'lg',
 }) => {
 	const { addItemCart } = useContext(ShoppingContext);
+	const [buttonText, setButtonText] = useState(addComic);
 
-	const handleAddItem = () => addItemCart({
-		total: price,
-		imageUrl: src,
-		title,
-		price,
-		idMarvel: id,
-		quantity: 1
-	})
+	const handleAddItem = () => {
+		addItemCart({
+			total: price,
+			imageUrl: src,
+			title,
+			price,
+			idMarvel: id,
+			quantity: 1,
+		});
+
+		setButtonText(comicAdded);
+
+		setTimeout(() => {
+			setButtonText(addComic);
+		}, 2500);
+	}
 
 	return (
 		<div className={styles.info}>
 			<Title size={defaultSize} className={styles.title}>
 				{title}
 			</Title>
-			<Paragraph content={`Calificación: ${rating}`} size={ratingSize} className={styles.rating} />
-			<Image src={src} alt={title} className={styles.img}/>
-      <Price
+			<Paragraph
+				content={`Calificación: ${rating}`}
+				size={ratingSize}
+				className={styles.rating}
+			/>
+			<Image src={src} alt={title} className={styles.img} />
+			<Price
 				currency={currency}
 				price={price}
 				className={styles.comicInfoPrice}
 				size={priceSize}
 			/>
-      <Button
-					className={styles.buyButton}
-					size='md'
-					color='green'
-					onClick={handleAddItem}
-					children='AGREGAR AL CARRITO'
-				/>
+			<Button
+				className={styles.buyButton}
+				size='md'
+				color='green'
+				onClick={handleAddItem}
+			>
+				{buttonText}
+			</Button>
 		</div>
 	);
 };
