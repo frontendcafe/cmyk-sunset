b452736d1d56032a12bda99f45413bfc4b72c4ca
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-05-07 00:04:55 -0300

    agrego validacion de sesion

--- pages/payment/index.js
+++ pages/payment/index.js
@@ -1,7 +1,21 @@
+import {useContext, useEffect} from 'react';
+import { useRouter } from 'next/router';
+
 import Container from 'components/atoms/Container/index';
 import CartForm from 'components/molecules/CartForm/index';
+import {DataContext} from 'context/DataContext';
 
 export default function Checkout() {
+  const router = useRouter();
+  const { isLogged } = useContext(DataContext);
+
+  useEffect(()=> {
+    if (!isLogged()) router.push({
+      pathname: '/login',
+      query: { redirectTo: 'payment' },
+    });
+  }, []);
+
   return (
     <Container>
       <CartForm />
